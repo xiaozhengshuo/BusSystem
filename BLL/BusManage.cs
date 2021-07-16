669c3a7cdb5374eadc426026e04ffceecc49b12a
xiaozhengshuo <57279233+xiaozhengshuo@users.noreply.github.com>
2021-07-16 11:15:58 +0800

    业务逻辑层
    业务逻辑层(

---
+++ BLL/BusManage.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Models;
+using DAL;
+using System.Data;
+
+namespace BLL
+{
+   public class BusManage
+    {
+       public DataTable GetAllMembers()
+       {
+           try
+           {
+               BusService memberdal = new BusService();
+               return memberdal.GetAllMembers();
+           }
+           catch (Exception ex)
+           {
+               throw new Exception(ex.ToString());
+           }
+       }
+        public DataTable AddBus()
+       {
+           try
+           {
+               BusService memberdal = new BusService();
+               return memberdal.AddBus();
+           }
+           catch (Exception ex)
+           {
+               throw new Exception(ex.ToString());
+               
+           }
+       }
+
+        public DataTable DeletBus()
+        {
+            try
+            {
+                BusService deletdal = new BusService();
+                return deletdal.DelteBus();
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.ToString());
+            }
+        }
+    }
+}
---
+++ BLL/BusQueryManage.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Models;
+using DAL;
+namespace BLL
+{
+   public class BusQueryManage
+    {
+        public BusMember Getnumber(int number)
+        {
+            UserQueryService uqs = new UserQueryService();
+            return uqs.Getnumber(number);
+        }
+
+        public BusMember Getline(string line01,string line02)
+        {
+            UserQueryService uqs = new UserQueryService();
+            return uqs.Getline(line01,line02);
+        }
+    }
+}
