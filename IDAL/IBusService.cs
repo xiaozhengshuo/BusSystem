45c4c1422942d5518b2391bc064996dde5fec6a4
xiaozhengshuo <57279233+xiaozhengshuo@users.noreply.github.com>
2021-07-16 11:19:12 +0800

    接口
    接口

---
+++ IDAL/IBusService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using Models;
+namespace IDAL
+{
+    public interface IBusService
+    {
+        //获取所有公交路线信息
+        DataTable GetAllMembers();
+        //根据车辆编号获得路线信息
+        BusMember GetMemberByID(string id);
+        //添加信息
+        bool AddMember(BusMember member);
+        //修改信息
+        bool UpdateMember(BusMember member);
+        //根据编号删除信息
+        bool DeleteMember(string id);
+    }
+}
